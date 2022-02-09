2c4c566d47bb17913b2ed7b1897de37dbf204e58
matehus-henrique <53536966+matehus-henrique@users.noreply.github.com>
2022-02-09 18:15:41 -0300

    Delete site_amazon directory

--- site_amazon/site/models/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	try {
-	  $connect = new mysqli("127.0.0.1", "root", "", "barsystem_contato");
-	  $connect->set_charset("utf8mb4");
-	} catch(Exception $e) {
-	  error_log($e->getMessage());
-	  exit('Erro ao tentar conectar ao banco de dados');
-	}
